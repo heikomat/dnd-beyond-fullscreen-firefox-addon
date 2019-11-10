a43297752cd8249ec74dc98ee7b60bb77f1d3be9
Heiko Mathes <heiko.mathes@gmail.com>
2019-11-10 12:21:56 +0100

    :sparkles: add hide-nav button

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.2
+Adds new "Hide Nav" button to remove the searchbar and the nav-bar. Not
+guaranteed to look correct on all sites, but is tested with the character-sheet page
+
+# 0.1
+Initial release with the fullscreen button
--- add-fullscreen-buttons.js
+++ add-fullscreen-buttons.js
@@ -1,63 +1,100 @@
-const addButtonToSidebar = () => {
+const addButtonToSidebar = (id, title, callback) => {
   const targetElement = document.querySelector('[role=navigation] ul');
   const sidebarExists = targetElement !== undefined && targetElement !== null;
   if (!sidebarExists) {
     return;
   }
 
-  const existingFullscreenButton = document.getElementById('fullscree-button-sidebar');
-  const fullscreenButtonAlreadyExists = existingFullscreenButton !== undefined && existingFullscreenButton !== null;
-  if (fullscreenButtonAlreadyExists) {
+  const existingButton = document.getElementById(id);
+  const buttonAlreadyExists = existingButton !== undefined && existingButton !== null;
+  if (buttonAlreadyExists) {
     return;
   }
 
-  const fullscreenButtonText = document.createElement('span');
-  fullscreenButtonText.className = 'b-list-label';
-  fullscreenButtonText.innerHTML = 'Fullscreen';
+  const buttonText = document.createElement('span');
+  buttonText.className = 'b-list-label';
+  buttonText.innerHTML = title;
   
-  const fullscreenButtonAnchor = document.createElement('a');
-  fullscreenButtonAnchor.appendChild(fullscreenButtonText);
+  const buttonAnchor = document.createElement('a');
+  buttonAnchor.appendChild(buttonText);
   
-  const fullscreenButton = document.createElement('li');
-  fullscreenButton.className = 'b-list-item p-nav-item';
-  fullscreenButton.setAttribute('id', 'fullscree-button-sidebar');
-  fullscreenButton.addEventListener('click', () => {
-    document.documentElement.mozRequestFullScreen();
-  })
-  fullscreenButton.appendChild(fullscreenButtonAnchor);
-  targetElement.appendChild(fullscreenButton)
+  const button = document.createElement('li');
+  button.className = 'b-list-item p-nav-item';
+  button.setAttribute('id', id);
+  button.addEventListener('click', callback)
+  button.appendChild(buttonAnchor);
+  targetElement.appendChild(button)
 }
 
-const addButtonToMenubar = () => {
+const addButtonToMenubar = (id, title, callback) => {
   const targetElement = document.querySelector('[role=navigation] .mm-navbar__menu-list');
   const sidebarExists = targetElement !== undefined && targetElement !== null;
   if (!sidebarExists) {
     return;
   }
 
-  const existingFullscreenButton = document.getElementById('fullscree-button-menubar');
-  const fullscreenButtonAlreadyExists = existingFullscreenButton !== undefined && existingFullscreenButton !== null;
-  if (fullscreenButtonAlreadyExists) {
+  const existingButton = document.getElementById(id);
+  const buttonAlreadyExists = existingButton !== undefined && existingButton !== null;
+  if (buttonAlreadyExists) {
     return;
   }
 
-  const fullscreenButtonText = document.createElement('span');
-  fullscreenButtonText.className = 'mm-nav-item__label mm-nav-item__label--link';
-  fullscreenButtonText.innerHTML = 'Fullscreen';
+  const buttonText = document.createElement('span');
+  buttonText.className = 'mm-nav-item__label mm-nav-item__label--link';
+  buttonText.innerHTML = title;
 
-  const fullscreenButton = document.createElement('li');
-  fullscreenButton.className = 'mm-nav-item';
-  fullscreenButton.setAttribute('id', 'fullscree-button-menubar');
-  fullscreenButton.addEventListener('click', () => {
-    document.documentElement.mozRequestFullScreen();
-  });
-  fullscreenButton.appendChild(fullscreenButtonText);
-  targetElement.appendChild(fullscreenButton);
+  const button = document.createElement('li');
+  button.className = 'mm-nav-item';
+  button.setAttribute('id', id);
+  button.addEventListener('click', callback);
+  button.appendChild(buttonText);
+  targetElement.appendChild(button);
+}
+
+const goFullscreen = () => {
+  document.documentElement.mozRequestFullScreen();
+}
+
+const removeElementByQuerySelector = (querySelector) => {
+  const element = document.querySelector(querySelector);
+  if (element !== undefined && element !== null) {
+    element.parentNode.removeChild(element);
+  }
+}
+
+const setElementStyle = (querySelector, styles) => {
+  const element = document.querySelector(querySelector);
+  if (element !== undefined && element !== null) {
+    for (const [style, value] of Object.entries(styles)) {
+      element.style[style] = value;
+    }
+  }
+}
+
+const a = alert;
+const hideNav = () => {
+  removeElementByQuerySelector('.site-bar');
+  removeElementByQuerySelector('[id=mega-menu-target]');
+  setElementStyle('header[role=banner]', {height: 0, overflow: 'hidden'});
+
+  // close the mobile burger menu
+  document.body.classList.remove('burger-open');
+
+  // move the content up to fill the space left by the navbar
+  setElementStyle('html body.responsive-enabled div#site-main', {'padding-top': '0px'});
+  setElementStyle('.ct-character-sheet-mobile__header', {'top': '0px'});
+
+  // update the background position to be just under the header
+  const sheet = window.document.styleSheets[0];
+  sheet.insertRule('@media (min-width: 1200px) {body {position: relative; background-position-y: 116px !important}}', sheet.cssRules.length);
+  sheet.insertRule('@media (max-width: 1200px) {body {position: relative; background-position-y: 94px !important}}', sheet.cssRules.length);
 }
 
 const addButtons = () => {
-  addButtonToSidebar();
-  addButtonToMenubar();
+  addButtonToSidebar('fullscreen-button-sidebar', 'Fullscreen', goFullscreen);
+  addButtonToMenubar('fullscreen-button-menubar', 'Fullscreen', goFullscreen);
+  addButtonToSidebar('hidenav-button-sidebar', 'Hide Nav', hideNav);
+  addButtonToMenubar('hidenav-button-menubar', 'Hide Nav', hideNav);
 }
 
 window.addEventListener('resize', addButtons);
